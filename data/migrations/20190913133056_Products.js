1c63dae316c0952ba882ef8f597c58c5daae6c92
MadisonViaLux <madison.s.mckown@gmail.com>
2019-09-13 15:41:02 -0700

    tables

--- data/migrations/20190913133056_Products.js
+++ data/migrations/20190913133056_Products.js
@@ -1,8 +1,33 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments();
+        tbl.text('name', 128)
+            .unique()
+            .notNullable();
+        tbl.text('description')
+        tbl.boolean('completed', false)
+    })
+    .createTable('resource', tbl => {
+        tbl.increments()
+        tbl.text('name', 128)
+            .unique()
+            .notNullable();
+        tbl.text('description')
+    })
+    .createTable('task', tbl => {
+        tbl.increments()
+        tbl.text('description')
+            .notNullable()
+        tbl.text('Additional Information')
+        tbl.boolean('completed', false)
+    })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resource')
+    .dropTableIfExists('task')
 };
--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const PORT = process.env.PORT || 7777;
 server.listen(PORT, () => {
   console.log(`\n\tListening on port ${PORT}...\n`);
 });
+
+
+
+// res.send(1) = True
+// res.send(0) = Flase
